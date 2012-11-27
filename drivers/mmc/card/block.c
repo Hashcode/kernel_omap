d6b0489476b943f278217d1b24bd8285667e2d98
Arve Hjønnevåg <arve@android.com>
2012-11-26 19:30:22 -0800

    mmc: block: Remove call to mmc_blk_set_blksize
    It no longer exists.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1957,9 +1957,6 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 	return 0;
 }
 
-static int
-mmc_blk_set_blksize(struct mmc_blk_data *md, struct mmc_card *card);
-
 static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 {
 	int ret;
@@ -1969,10 +1966,8 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	unsigned long flags;
 
 #ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
-	if (mmc_bus_needs_resume(card->host)) {
+	if (mmc_bus_needs_resume(card->host))
 		mmc_resume_bus(card->host);
-		mmc_blk_set_blksize(md, card);
-	}
 #endif
 
 	if (req && !mq->mqrq_prev->req)
